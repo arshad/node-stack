17d30743763ca5478721ce355232dba3f728db3c
Arshad Chummun <arshad@chummun.com>
2014-06-03 22:27:20 +0400

    Added express and created a basic app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.end('Hola');
+});
+
+app.listen(3000, function() {
+  console.log("Now listening at http://127.0.0.1:3000");
+});
