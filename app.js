afd9626d1521bdfd71c6b9c4663d3f870b7d8633
Arshad Chummun <arshad@chummun.com>
2014-06-08 17:49:34 +0400

    Added swig templating engine for views

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var config = require('./app/config/config');
+var swig = require('swig');
 var express = require('express');
 var app = express();
+
+// Use swig as the template engine.
+app.engine('swig.html', swig.renderFile);
+app.set('view engine', 'swig.html');
+app.set('views', __dirname + '/app/views');
 
 // Add routes.
 var routes = require('./app/routes')(app);
--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,6 @@
 var routes = module.exports = function(app) {
   // Root
   app.get('/', function(req, res) {
-    res.end('Hello World');
+    res.render('home/index');
   });
 }
